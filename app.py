4af97abfd4c0065170fd897789467fcfcee95dfa
Lou <lou@Lous-iMac.home>
2018-09-06 19:07:12 -0400

    added item & itemlist resources

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -6,15 +6,39 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-    def post(self):
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(iter([item for item in items if item['name'] == name]), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(iter([item for item in items if item['name'] == name]), None):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+        data = request.get_json()
+        item = {
+            "name": name,
+            "price": data['price']
+        }
+        items.append(item)
+        return item, 201
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
         pass
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Item, '/item/<string:name>')
 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
