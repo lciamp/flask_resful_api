b1122bf4657628f7985286b7dfac7c5fe0d05bd8
Lou <lou@Lous-iMac.home>
2018-09-06 15:29:13 -0400

    app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+    def get(self, name):
+        return {'student': name}
+
+    def post(self):
+        pass
+
+
+api.add_resource(Student, '/student/<string:name>')
+
+
+app.run(port=5000)
